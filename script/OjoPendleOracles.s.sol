7a31d2e1bc2dd17da2e43e11a5fa904dbcf56ed5
rbajollari <rbajollari@gmail.com>
2024-11-26 14:54:17 -0500

    feat: OjoPTOraclePriceAdapter

--- script/OjoPendleOracles.s.sol
+++ script/OjoPendleOracles.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {OjoPendleOracle} from "src/OjoPendleOracle.sol";
+import {OjoPTOraclePriceAdapter} from "src/OjoPTOraclePriceAdapter.sol";
 import {console} from "forge-std/console.sol";
 
 contract DeployOjoPendleOracle is Script {
@@ -17,5 +18,21 @@ contract DeployOjoPendleOracle is Script {
         vm.stopBroadcast();
 
         console.log("OjoPendleOracle Address:", address(ojoPendleOracle));
+    }
+}
+
+contract DeployOjoPTOraclePriceAdapter is Script {
+    function run() external {
+        address _ptoracle = vm.envAddress("PT_ORACLE");
+        address _market = vm.envAddress("MARKET");
+        string memory _description = vm.envString("DESCRIPTION");
+
+        vm.startBroadcast();
+
+        OjoPTOraclePriceAdapter ojoPTOraclePriceAdapter = new OjoPTOraclePriceAdapter(_ptoracle, _market, _description);
+
+        vm.stopBroadcast();
+
+        console.log("OjoPTOraclePriceAdapter Address:", address(ojoPTOraclePriceAdapter));
     }
 }
