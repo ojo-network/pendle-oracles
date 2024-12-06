00b1548867fe7dad20b335d77b5a0a2c9bcb9f38
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2024-12-06 23:50:13 +0100

    fix: Three Sigma audit fixes (#2)

--- src/OjoPTOraclePriceAdapter.sol
+++ src/OjoPTOraclePriceAdapter.sol
@@ -13,6 +13,14 @@ contract OjoPTOraclePriceAdapter is MinimalAggregatorV3Interface {
 
     string public description;
 
+    /// @notice thrown when the Pendle market decimals are not 18
+    uint256 internal constant OjoPTOraclePriceAdapter__MarketDecimalsNotSupported = 10_000;
+
+    /// @notice thrown when the Pendle market Oracle has not been initialized yet
+    uint256 internal constant OjoPTOraclePriceAdapter__MarketNotInitialized = 10_001;
+
+    error OjoPTOraclePriceAdapterError(uint256 errorId_);
+
     constructor(address _ptoracle, address _market, string memory _description) {
         require(_ptoracle != address(0), "_ptoracle zero address");
         require(_market != address(0), "_market zero address");
@@ -20,6 +28,23 @@ contract OjoPTOraclePriceAdapter is MinimalAggregatorV3Interface {
         PTOracle = _ptoracle;
         market = _market;
         description = _description;
+        if (MinimalAggregatorV3Interface(_market).decimals() != decimals) {
+            revert OjoPTOraclePriceAdapterError(OjoPTOraclePriceAdapter__MarketDecimalsNotSupported);
+        }
+
+        IPTOracle oracle = IPTOracle(PTOracle);
+
+        {
+            (bool increaseCardinalityRequired_,, bool oldestObservationSatisfied_) = oracle.getOracleState(
+                market,
+                900 // 15 mins twap duration
+            );
+            if (increaseCardinalityRequired_ || !oldestObservationSatisfied_) {
+                // ensure pendle market Oracle is ready and initialized see
+                // https://docs.pendle.finance/Developers/Oracles/HowToIntegratePtAndLpOracle
+                revert OjoPTOraclePriceAdapterError(OjoPTOraclePriceAdapter__MarketNotInitialized);
+            }
+        }
     }
 
     /// @inheritdoc MinimalAggregatorV3Interface
