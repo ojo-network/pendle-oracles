1b9e9ed55cfd985132cc0838740854fb57beeb25
rbajollari <rbajollari@gmail.com>
2024-12-06 14:18:38 -0500

    fix: Three Sigma audit fixes

--- src/OjoPTOraclePriceAdapter.sol
+++ src/OjoPTOraclePriceAdapter.sol
@@ -5,13 +5,18 @@ import {IPTOracle} from "./interfaces/IPTOracle.sol";
 import {MinimalAggregatorV3Interface} from "./interfaces/MinimalAggregatorV3Interface.sol";
 
 contract OjoPTOraclePriceAdapter is MinimalAggregatorV3Interface {
-    uint8 public constant decimals = 18;
+    uint8 public decimals;
 
     address public immutable PTOracle;
 
     address public immutable market;
 
     string public description;
+
+    /// @notice thrown when the Pendle market Oracle has not been initialized yet
+    uint256 internal constant PendleOracle__MarketNotInitialized = 10_000;
+
+    error OjoPTOraclePriceAdapterError(uint256 errorId_);
 
     constructor(address _ptoracle, address _market, string memory _description) {
         require(_ptoracle != address(0), "_ptoracle zero address");
@@ -20,6 +25,7 @@ contract OjoPTOraclePriceAdapter is MinimalAggregatorV3Interface {
         PTOracle = _ptoracle;
         market = _market;
         description = _description;
+        decimals = MinimalAggregatorV3Interface(_market).decimals();
     }
 
     /// @inheritdoc MinimalAggregatorV3Interface
@@ -27,6 +33,18 @@ contract OjoPTOraclePriceAdapter is MinimalAggregatorV3Interface {
     /// @dev Silently overflows if `price`'s average is greater than `type(int256).max`.
     function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80) {
         IPTOracle oracle = IPTOracle(PTOracle);
+
+        {
+            (bool increaseCardinalityRequired_,, bool oldestObservationSatisfied_) = oracle.getOracleState(
+                market,
+                900 // 15 mins twap duration
+            );
+            if (increaseCardinalityRequired_ || !oldestObservationSatisfied_) {
+                // ensure pendle market Oracle is ready and initialized see
+                // https://docs.pendle.finance/Developers/Oracles/HowToIntegratePtAndLpOracle
+                revert OjoPTOraclePriceAdapterError(PendleOracle__MarketNotInitialized);
+            }
+        }
 
         uint256 price = oracle.getPtToAssetRate(
             market,
--- src/interfaces/IPTOracle.sol
+++ src/interfaces/IPTOracle.sol
@@ -3,4 +3,12 @@ pragma solidity >=0.5.0;
 
 interface IPTOracle {
     function getPtToAssetRate(address market, uint32 duration) external view returns (uint256);
+
+    function getOracleState(
+        address market,
+        uint32 duration
+    )
+        external
+        view
+        returns (bool increaseCardinalityRequired, uint16 cardinalityRequired, bool oldestObservationSatisfied);
 }
