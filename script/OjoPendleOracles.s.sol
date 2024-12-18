b4181f854bc2771706031d056970f548854652ba
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2024-12-18 12:37:33 -0500

    fix: evm version in config (#3)

--- script/OjoPendleOracles.s.sol
+++ script/OjoPendleOracles.s.sol
@@ -5,6 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {OjoPendleOracle} from "src/OjoPendleOracle.sol";
 import {OjoPTOraclePriceAdapter} from "src/OjoPTOraclePriceAdapter.sol";
 import {console} from "forge-std/console.sol";
+import {IPTOracle} from "src/interfaces/IPTOracle.sol";
 
 contract DeployOjoPendleOracle is Script {
     function run() external {
@@ -34,5 +35,24 @@ contract DeployOjoPTOraclePriceAdapter is Script {
         vm.stopBroadcast();
 
         console.log("OjoPTOraclePriceAdapter Address:", address(ojoPTOraclePriceAdapter));
+    }
+}
+
+contract CheckOracleState is Script {
+    function run() external {
+        address _ptoracle = vm.envAddress("PT_ORACLE");
+        address _market = vm.envAddress("MARKET");
+
+        IPTOracle oracle = IPTOracle(_ptoracle);
+
+        (bool increaseCardinalityRequired_, uint16 cardinalityRequired_, bool oldestObservationSatisfied_) = oracle
+            .getOracleState(
+            _market,
+            900 // 15 mins twap duration
+        );
+
+        console.log("increaseCardinalityRequired", increaseCardinalityRequired_);
+        console.log("cardinalityRequired", cardinalityRequired_);
+        console.log("oldestObservationSatisfied", oldestObservationSatisfied_);
     }
 }
